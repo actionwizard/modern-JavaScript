3e38808917c9572fcb14fb1eb526990790616e81
Anish Kumar <anishykumar42@gmail.com>
2023-07-15 13:20:27 +0530

    learned var, let using the file.js

--- file.js
+++ file.js
@@ -1,9 +1,11 @@
-//Using the array.map method, what if we use strict
-"use strict";
-const arr = [2, 4, 8, 6];
+function add(s) {
+  a = undefined;
+  sum = a;
+  {
+    var a = undefined;
+  }
+  console.log(sum, s, a);
+  return s;
+}
 
-const result = arr.map(function (number, index, array) {
-  return 1;
-});
-
-console.log("result", result);
+add(undefined);

8e920589ddca8e30243e57e134eada1a74f5622d
Anish Kumar <anishykumar42@gmail.com>
2023-09-08 15:50:09 +0000

    edits

--- chaiAurCode/01_basics/dataTypeSummary.js
+++ chaiAurCode/01_basics/dataTypeSummary.js
@@ -2,10 +2,15 @@
 There are basically two divisions of datatypes in JS
 1. Primitive
     -no methods or properties
+    
     -immutable (The variable may be reassigned to a new value, 
                 but the existing value can not be changed 
                 in the ways that objects, arrays, and functions can be altered.)
-    -we have wrappers which create a temporary object of the primitive datatype and we get 
+
+    -all primitives are declared in the stack, and whenever we access something in the stack
+    
+    -we have wrappers which create a temporary object of the primitive datatype and we actually do
+    using call by value concept, i.e. a copy of the variable value is given. example below
     access to a few properties of the object. eg string 
     -number, string, boolean, symbol, bigint, null **(return type is object), undefined
     {Conceptually, undefined => absence of a value, while null => absence of an object}

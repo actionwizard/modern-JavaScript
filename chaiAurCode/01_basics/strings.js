b780fed99cd7b738ef4190a47ca6165b9434e1bd
Anish Kumar <anishykumar42@gmail.com>
2023-09-09 15:23:17 +0000

    string basic started

---
+++ chaiAurCode/01_basics/strings.js
@@ -0,0 +1,7 @@
+//Basics about string
+let userName = "akshay khanna";
+console.log(userName);
+let part = userName.slice(0, 6);
+console.log(part);
+let userName2 = userName;
+console.log(userName2.split());

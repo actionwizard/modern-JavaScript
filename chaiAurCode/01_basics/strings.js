03ddacd113de1cf9e0acb878aa759afc765f1c3a
Anish Kumar <anishykumar42@gmail.com>
2023-09-12 16:42:37 +0000

    a few string related functions

--- chaiAurCode/01_basics/strings.js
+++ chaiAurCode/01_basics/strings.js
@@ -1,7 +1,31 @@
 //Basics about string
 let userName = "akshay khanna";
-console.log(userName);
-let part = userName.slice(0, 6);
-console.log(part);
-let userName2 = userName;
-console.log(userName2.split());
+
+//string can be created in any way you like
+let str1 = "some value of string";
+let str2 = new String("loremipsum");
+
+// console.log(str1.length);
+// console.log(str2.length);
+
+//ye str.toUpperCase wala tarika jo hai wo original string me change nahi karta hai
+// console.log(str1.toUpperCase());
+// console.log(str1);
+// console.log(typeof str2);
+// console.log(str2.toUpperCase());
+// console.log(str2.valueOf());
+
+//Maine ek string banayi aur ek string object banayi dono par operation perform karke dekha
+
+//Some functions of string
+// console.log(str1.charAt(5));
+// console.log(str1.indexOf('asd'));
+// console.log(str1.indexOf('a'));
+
+//Substring and slice me difference itna hai bas ki slice me -ve start denge to wo last se
+//start karega substring lena
+let subStr = str1.substring(0, 5);
+console.log(subStr);
+
+let sliceStr = str1.slice(-1); //This will take values from the end
+console.log(sliceStr);

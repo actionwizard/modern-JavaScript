98ddc691636696fde9a3246c683d71d0010ca58a
Anish Kumar <anishykumar42@gmail.com>
2023-09-26 17:24:20 +0000

    string lesson done

--- chaiAurCode/01_basics/strings.js
+++ chaiAurCode/01_basics/strings.js
@@ -25,8 +25,34 @@ let str2 = new String("loremipsum");
 //Substring and slice me difference itna hai bas ki slice me -ve start denge to wo last se
 //start karega substring lena
 let subStr = str1.substring(0, 5);
-console.log(subStr);
+// console.log(subStr);
 
 let sliceStr = str1.slice(-6,-2); //This will take values from the end
-console.log(sliceStr);
+// console.log(sliceStr);
 
+
+//Trim and Replace Method
+//Trim -> ye function string ke starting ke aur end ke spaces ko remove kardeta hai
+//only white spaces and line terminators (/n)
+
+let spacesWaliString = "      gekko is insane     ";
+let lineTerminatorsWaliString = "\t \t even yoru is great";
+// console.log(lineTerminatorsWaliString);
+// console.log(lineTerminatorsWaliString.trimEnd());
+
+//To pata ye laga ki \n \t dono ko remove kardeta hai trim method shuru me ya end me
+
+//Replace method se ham string me kisi specific part ko replace kr sakte hai kisi dusre se
+let url = `https://anishKR.com/anish%20kumar`;
+let url2 = `https://anishKR.com/anish kumar`;
+
+//In the above we can replace %20 by - or the spaces by dash
+
+console.log(url.replace('%20','-'));
+console.log(url2.replace(' ','-'));
+
+
+
+
+//FOR MORE EXPERTISE ON STRINGS YOU JUST HAVE TO GO TO STRING MDN
+//NOTE -> use the backticks way of declaring the strings

9c394696bf8dcf077fb355833dc758eb9b3021f4
Anish Kumar <anishykumar42@gmail.com>
2023-09-27 17:53:11 +0000

    learning dates in js

---
+++ chaiAurCode/01_basics/dates.js
@@ -0,0 +1,15 @@
+//Dates in js are a bit complex but very useful, date is a type of object in js and
+//date calculation is done from a starting point known as an epoch time or unix time
+//the date is 1 Jan 1970, the time is tracked in miliseconds
+
+let myDate1 = new Date(-1);
+let myDate2 = new Date(0);
+let myDate3 = new Date(1);
+let myDate4 = new Date();
+
+console.log(myDate1.toString());
+console.log(myDate2.toString());
+console.log(myDate3.toString());
+console.log(myDate4.toString());
+
+//and there are many more methods available for this one, you can work on it on your own

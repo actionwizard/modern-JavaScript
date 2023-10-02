5183b8456e35d8d0ee9245e933b1b9b5b04a6238
Anish Kumar <anishykumar42@gmail.com>
2023-10-02 22:59:36 +0000

    dom basic

---
+++ chaiAurCode/06_dom/one.js
@@ -0,0 +1,11 @@
+/*
+    getElementById() ==> is method ka use karte hai basics me
+
+    title = getELementById('title');
+
+    title.innerText => ye jo hai na wo text deta hai jo ki webpage par us element ke ander pada hai 
+    title.textContent   => ye bhi same upar wale ki tarh hi hai, bas itna fark hai ki ye sara content dedega even if the content
+                           of the element is not visible.
+    title.innerHTML => ye hame us element ke ander koi html tag likha hai wo wo bhi dedeta hai, bakio me ander agar html element
+                        hai to bhi sirf text text milega.
+*/
